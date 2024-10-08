2c3d6a24944fd4bc38e595876d742358705ac1a7
Muktim <muktim.coskuner@outlook.com>
2024-10-08 01:44:32 +0100

    Just commenting unnecessary checks. I was running tests

--- PARSE/parse_main.c
+++ PARSE/parse_main.c
@@ -6,7 +6,7 @@
 /*   By: mcoskune <mcoskune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 15:25:00 by mcoskune          #+#    #+#             */
-/*   Updated: 2024/10/07 22:48:24 by mcoskune         ###   ########.fr       */
+/*   Updated: 2024/10/08 01:22:22 by mcoskune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,7 +111,7 @@ int	parse_main(t_msh *msh)
 	}
 	if (create_modified(msh, msh->parse) == 1)
 		return (1);
-	printf("Modified string is: %s\n", msh->parse->modified);
+	// printf("Modified string is: %s\n", msh->parse->modified);
 	
 	parse_tokenize(msh, msh->parse);
 
@@ -133,25 +133,25 @@ int	parse_main(t_msh *msh)
 
 	make_pexe(msh, msh->parse);
 
-	// t_pexe *temp = msh->pexe;
-	// while (temp != NULL)
-	// {
-	// 	int	i = 0;
-	// 	while (temp->cmd[i] != '\0')
-	// 	{
-	// 		if (temp->cmd[i] == ' ')
-	// 			temp->cmd[i] = '#';
-	// 		i++;
-	// 	}
-	// 	printf("PEXE-TYPE: %d\n", temp->type);
-	// 	printf("PEXE-CMD: %s\n", temp->cmd);
-	// 	printf("PEXE-GROUP_ID: %d\n", temp->group_id);
-	// 	printf("PEXE-PRIORITY: %d\n", temp->p_index);
-	// 	printf("PEXE-MUK_NOTE: %d\n", temp->muk_note);
-	// 	printf("PEXE-TEMP: %s\n\n", temp->temp);
+	t_pexe *temp = msh->pexe;
+	while (temp != NULL)
+	{
+		int	i = 0;
+		while (temp->cmd[i] != '\0')
+		{
+			if (temp->cmd[i] == ' ')
+				temp->cmd[i] = '#';
+			i++;
+		}
+		printf("PEXE-TYPE: %d\n", temp->type);
+		printf("PEXE-CMD: %s\n", temp->cmd);
+		printf("PEXE-GROUP_ID: %d\n", temp->group_id);
+		printf("PEXE-PRIORITY: %d\n", temp->p_index);
+		printf("PEXE-MUK_NOTE: %d\n", temp->muk_note);
+		printf("PEXE-TEMP: %s\n\n", temp->temp);
 
-	// 	temp = temp->next;
-	// }
+		temp = temp->next;
+	}
 
 	return (0);
 }
