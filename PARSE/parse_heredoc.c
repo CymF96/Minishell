2ea34ed84c168076cbe4b95104add50ae294ac2a
Muktim <muktim.coskuner@outlook.com>
2024-10-15 20:53:31 +0100

    committing to pull any updates

--- PARSE/parse_heredoc.c
+++ PARSE/parse_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: mcoskune <mcoskune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 17:36:54 by mcoskune          #+#    #+#             */
-/*   Updated: 2024/10/14 20:34:01 by mcoskune         ###   ########.fr       */
+/*   Updated: 2024/10/15 20:50:25 by mcoskune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,10 +67,12 @@ void	remove_quotes(char *str, int len, char *delim)
 			delim[j++] = str[i++];
 	}
 	delim[j] = '\0';
+	printf("DELIM IS %s\n\n", delim);
 }
 
 static void	heredoc_specials(t_msh *msh, int *i, int *flag)
 {
+	
 	while (msh->input[*i] != ' ' && msh->input[*i] != '\t' && msh->input[*i] \
 		!= '\0' && (check_special(msh->input, i) == REGULAR || \
 		check_special(msh->input, i) == DOLLAR || check_special(msh->input, i) \
@@ -97,8 +99,10 @@ int	handle_heredoc(t_msh *msh, int *i)
 	while (msh->input[*i] == ' ' || msh->input[*i] == '\t')
 		(*i)++;
 	start = (*i);
+	printf("HELLOOOOO\n");
 	heredoc_specials(msh, i, &flag);
 	delim = malloc(sizeof(char) * (*i - start + 1));
+	printf("START IS: %d\nEND IS: %d\n", start, *i);
 	remove_quotes(&msh->input[start], *i - start, delim);
 	if (get_here_doc(msh, delim, flag) || msh->interrupted)
 	{
